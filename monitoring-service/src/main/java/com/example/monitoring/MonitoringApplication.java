7739131d121f86449eaf6ab3d3296a96079afd44
arathod-gd <arathod@griddynamics.com>
2026-07-06 16:56:10 +0530

    monitoring service

---
+++ monitoring-service/src/main/java/com/example/monitoring/MonitoringApplication.java
@@ -0,0 +1,15 @@
+package com.example.monitoring;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class MonitoringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringApplication.class, args);
+    }
+}
